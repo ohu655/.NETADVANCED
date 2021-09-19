f255d1c5455bc0fd863485667fa78d25725de7f6
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-09-19 21:14:35 +0300

    Add files via upload

---
+++ 6. Songs Queue/6. Songs Queue/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine()
+                .Split(", ");
+
+            Queue<string> songsPlaylist = new Queue<string>(songs);
+
+            while (songsPlaylist.Any())
+            {
+                string command = Console.ReadLine();
+                
+
+                if (command.Contains("Play"))
+                {
+                    songsPlaylist.Dequeue();
+                }
+                else if (command.Contains("Add"))
+                {
+                    string songName = command.Substring(4);
+
+                    if (!songsPlaylist.Contains(songName))
+                    {
+                        songsPlaylist.Enqueue(songName);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{songName} is already contained!");
+                    }                   
+                }
+                else if (command.Contains("Show"))
+                {
+                    Console.WriteLine($"{string.Join(", ", songsPlaylist)}");
+                }               
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
