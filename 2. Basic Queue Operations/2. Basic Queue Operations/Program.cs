841833884366ea212715282f70195aa907690b1c
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-09-19 15:19:08 +0300

    Add files via upload

---
+++ 2. Basic Queue Operations/2. Basic Queue Operations/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+
+            int numbersToEnque = numbers[0];
+            int numbersToDeque = numbers[1];
+            int numberToPeek = numbers[2];
+
+            int[] numbersInQueue = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < numbersToEnque; i++)
+            {
+                int currentNumber = numbersInQueue[i];
+                queue.Enqueue(currentNumber);
+            }
+
+            for (int i = 0; i < numbersToDeque; i++)
+            {
+                queue.Dequeue();
+            }
+
+            int smallestNumber = int.MaxValue;
+            while (queue.Any())
+            {
+                if (queue.Peek() == numberToPeek)
+                {
+                    Console.WriteLine("true");
+                    return;
+                }
+                else
+                {
+                    int number = queue.Dequeue();
+
+                    if (smallestNumber > number)
+                    {
+                        smallestNumber = number;
+                    }
+                }
+
+                if (queue.Count == 0)
+                {
+                    Console.WriteLine(smallestNumber);
+                    return;
+                }
+            }
+
+            Console.WriteLine(0);
+        }
+    }
+}
---
+++ 3. Maximum and Minimum Element/3. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Maximum_and_Minimum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+
+            int maxElement = int.MinValue;
+            int minElement = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+
+                string[] parts = line.Split();
+                string command = parts[0];
+
+                if (command == "1")
+                {
+                    int numberToPush = int.Parse(parts[1]);
+
+                    stack.Push(numberToPush);
+
+                    if (minElement > stack.Peek())
+                    {
+                        minElement = stack.Peek();
+                    }
+
+                    if (maxElement < stack.Peek())
+                    {
+                        maxElement = stack.Peek();
+                    }
+                }
+                else if (command == "2")
+                {
+                    if (stack.Any())
+                    {
+                        int elementToRemove = stack.Pop();
+
+                        if (minElement == elementToRemove)
+                        {
+                            minElement = int.MaxValue;
+                        }
+
+                        if (maxElement == elementToRemove)
+                        {
+                            maxElement = int.MinValue;
+                        }
+                    }         
+                }
+                else if (command == "3")
+                {
+                    if (stack.Any())
+                    {
+                        Console.WriteLine(maxElement);
+                    }
+                    
+                }
+                else if (command == "4")
+                {
+                    if (stack.Any())
+                    {
+                        Console.WriteLine(minElement);
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine($"{string.Join(", ", stack)}");
+        }
+    }
+}
