31fd88634f77236ac93e193a9eeb10310f221d17
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-09-24 22:40:59 +0300

    Add files via upload

---
+++ 6. Jagged-Array Modification/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] numbers = new int[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                numbers[row] = new int[input.Length];
+
+                for (int col = 0; col < input.Length; col++)
+                {
+                    numbers[row][col] = input[col];
+                }
+            }
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "END")
+                {
+                    break;
+                }
+
+                string[] parts = line.Split();
+                string command = parts[0];
+                int row = int.Parse(parts[1]);
+                int col = int.Parse(parts[2]);
+                int value = int.Parse(parts[3]);
+
+                if (row >= numbers.Length || row < 0 || col >= numbers[row].Length || col < 0)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    continue;
+                }
+
+                if (command == "Add")
+                {
+
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        for (int j = 0; j < numbers[i].Length; j++)
+                        {
+                            if (i == row && j == col)
+                            {
+                                numbers[i][j] += value;
+                            }
+                        }
+                    }
+                }
+                else if (command == "Subtract")
+                {
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        for (int j = 0; j < numbers[i].Length; j++)
+                        {
+                            if (i == row && j == col)
+                            {
+                                numbers[i][j] -= value;
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers[i].Length; j++)
+                {
+                    Console.Write($"{numbers[i][j]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
+
---
+++ 7. Pascal Triangle/7. Pascal Triangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _7._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            long[][] numbers = new long[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = new long[i + 1];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i][0] = 1;
+                numbers[i][numbers[i].Length - 1] = 1;
+
+                for (int j = 1; j < numbers[i].Length - 1; j++)
+                {
+                    numbers[i][j] = numbers[i - 1][j - 1] + numbers[i - 1][j];
+                }
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers[i].Length; j++)
+                {
+                    Console.Write($"{numbers[i][j]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
