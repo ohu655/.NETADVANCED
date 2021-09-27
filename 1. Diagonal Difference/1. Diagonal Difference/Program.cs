083a5dfdd379e8a9d9651fe91404f2b8fecc7df6
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-09-27 23:21:52 +0300

    Add files via upload

---
+++ 1. Diagonal Difference/1. Diagonal Difference/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] diagonals = new int[n, n];
+
+            for (int row = 0; row < diagonals.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < diagonals.GetLength(1); col++)
+                {
+                    diagonals[row, col] = input[col];
+                }
+            }
+
+            int firstDiagonalSum = 0;
+            int secondDiagonalSum = 0;
+
+            for (int row = 0; row < diagonals.GetLength(0); row++)
+            {
+                firstDiagonalSum += diagonals[row, row];
+            }
+
+            for (int row = 0; row < diagonals.GetLength(0); row++)
+            {
+                secondDiagonalSum += diagonals[row, (diagonals.GetLength(0) -1) - row];              
+            }
+
+            Console.WriteLine(Math.Abs(firstDiagonalSum - secondDiagonalSum));
+        }
+    }
+}
---
+++ 2. 2X2 Squares in Matrix/2. 2X2 Squares in Matrix/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._2X2_Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            char[,] symbols = new char[rows, cols];
+
+            for (int row = 0; row < symbols.GetLength(0); row++)
+            {
+                char[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToArray();
+
+                for (int col = 0; col < symbols.GetLength(1); col++)
+                {
+                    symbols[row, col] = input[col];
+                }
+            }
+
+            int counter = 0;
+
+
+            for (int row = 0; row < symbols.GetLength(0) -1; row++)
+            {
+                for (int col = 0; col < symbols.GetLength(1) -1; col++)
+                {
+                    if (symbols[row, col] == symbols[row, col + 1])
+                    {
+                        if (symbols[row, col + 1] == symbols[row + 1, col])
+                        {
+                            if (symbols[row + 1, col] == symbols[row + 1, col + 1])
+                            {
+                                counter++;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
