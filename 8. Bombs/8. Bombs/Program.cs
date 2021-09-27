39e9a2de058e890bb81aba15b27d4ae539bf1ec3
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-09-27 23:26:32 +0300

    Add files via upload

---
+++ 8. Bombs/8. Bombs/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+
+namespace _8._Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] bombs = new int[n, n];
+
+            for (int row = 0; row < bombs.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < bombs.GetLength(1); col++)
+                {
+                    bombs[row, col] = input[col];
+                }
+            }
+        
+            string line = Console.ReadLine();
+            string[] parts = line.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                string[] elementParts = parts[i].Split(",");
+                int bombRows = int.Parse(elementParts[0]);
+                int bombCols = int.Parse(elementParts[1]);
+                int bombPower = bombs[bombRows, bombCols];
+
+                if (bombPower > 0)
+                {
+
+                    if (IsInside(bombs, bombRows - 1, bombCols - 1) && bombs[bombRows - 1, bombCols - 1] > 0)
+                    {
+                        bombs[bombRows - 1, bombCols - 1] -= bombs[bombRows, bombCols];
+                    }
+
+                    if (IsInside(bombs, bombRows - 1, bombCols) && bombs[bombRows - 1, bombCols] > 0)
+                    {
+                        bombs[bombRows - 1, bombCols] -= bombs[bombRows, bombCols];
+                    }
+
+                    if (IsInside(bombs, bombRows - 1, bombCols + 1) && bombs[bombRows - 1, bombCols + 1] > 0)
+                    {
+                        bombs[bombRows - 1, bombCols + 1] -= bombs[bombRows, bombCols];
+                    }
+
+                    if (IsInside(bombs, bombRows, bombCols - 1) && bombs[bombRows, bombCols - 1] > 0)
+                    {
+                        bombs[bombRows, bombCols - 1] -= bombs[bombRows, bombCols];
+                    }
+
+                    if (IsInside(bombs, bombRows, bombCols + 1) && bombs[bombRows, bombCols + 1] > 0)
+                    {
+                        bombs[bombRows, bombCols + 1] -= bombs[bombRows, bombCols];
+                    }
+
+                    if (IsInside(bombs, bombRows + 1, bombCols - 1) && bombs[bombRows + 1, bombCols - 1] > 0)
+                    {
+                        bombs[bombRows + 1, bombCols - 1] -= bombs[bombRows, bombCols];
+                    }
+
+                    if (IsInside(bombs, bombRows + 1, bombCols) && bombs[bombRows + 1, bombCols] > 0)
+                    {
+                        bombs[bombRows + 1, bombCols] -= bombs[bombRows, bombCols];
+                    }
+
+                    if (IsInside(bombs, bombRows + 1, bombCols + 1) && bombs[bombRows + 1, bombCols + 1] > 0)
+                    {
+                        bombs[bombRows + 1, bombCols + 1] -= bombs[bombRows, bombCols];
+                    }
+
+                    bombs[bombRows, bombCols] = 0;
+                }
+            }
+
+            int sum = 0;
+            int aliveCells = 0;
+
+            for (int row = 0; row < bombs.GetLength(0); row++)
+            {
+                for (int col = 0; col < bombs.GetLength(1); col++)
+                {
+                    if (bombs[row,col] > 0)
+                    {
+                        sum += bombs[row, col];
+                        aliveCells++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {aliveCells}");
+            Console.WriteLine($"Sum: {sum}");
+
+            for (int row = 0; row < bombs.GetLength(0); row++)
+            {
+                for (int col = 0; col < bombs.GetLength(1); col++)
+                {
+                    Console.Write($"{bombs[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        private static bool IsInside(int[,] bombs, int row, int col)
+        {
+            return row >= 0 && row < bombs.GetLength(0) && col >= 0 && col < bombs.GetLength(1);
+        }
+    }
+}
---
+++ 9. Miner/9. Miner/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Linq;
+
+namespace _9._Miner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] miner = new char[n, n];
+
+            string[] directions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int row = 0; row < miner.GetLength(0); row++)
+            {
+                char[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < miner.GetLength(1); col++)
+                {
+                    miner[row, col] = input[col];
+                }
+            }
+
+            int startRow = 0;
+            int startCol = 0;
+
+            for (int row = 0; row < miner.GetLength(0); row++)
+            {
+                for (int col = 0; col < miner.GetLength(1); col++)
+                {
+                    if (miner[row, col] == 's')
+                    {
+                        startRow = row;
+                        startCol = col;
+                    }
+                }
+            }
+
+            int allCoals = SumOfCoals(miner);
+            int collectedCoals = 0;
+
+            for (int i = 0; i < directions.Length; i++)
+            {
+                string command = directions[i];
+
+                if (command == "up")
+                {
+                    if (IsInside(miner, startRow - 1, startCol))
+                    {
+                        if (miner[startRow - 1, startCol] == 'e')
+                        {
+                            Console.WriteLine($"Game over! ({startRow - 1}, {startCol})");
+                            return;
+                        }
+                        else if (miner[startRow - 1, startCol] == 'c')
+                        {
+                            collectedCoals++;
+
+                            if (collectedCoals == allCoals)
+                            {
+                                Console.WriteLine($"You collected all coals! ({startRow - 1}, {startCol})");
+                                return;
+                            }
+                            else
+                            {
+                                miner[startRow - 1, startCol] = '*';
+                                startRow = startRow - 1;
+                            }   
+                        }
+                        else
+                        {
+                            startRow = startRow - 1;
+                        }                       
+                    }
+                }
+                else if (command == "down")
+                {
+                    if (IsInside(miner, startRow + 1, startCol))
+                    {
+                        if (miner[startRow + 1, startCol] == 'e')
+                        {
+                            Console.WriteLine($"Game over! ({startRow + 1}, {startCol})");
+                            return;
+                        }
+                        else if (miner[startRow + 1, startCol] == 'c')
+                        {
+                            collectedCoals++;
+
+                            if (collectedCoals == allCoals)
+                            {
+                                Console.WriteLine($"You collected all coals! ({startRow + 1}, {startCol})");
+                                return;
+                            }
+                            else
+                            {
+                                miner[startRow + 1, startCol] = '*';
+                                startRow = startRow + 1;
+                            }
+                        }
+                        else
+                        {
+                            startRow = startRow + 1;
+                        }
+                    }
+                }
+                else if (command == "right")
+                {
+                    if (IsInside(miner, startRow, startCol + 1))
+                    {
+                        if (miner[startRow, startCol + 1] == 'e')
+                        {
+                            Console.WriteLine($"Game over! ({startRow}, {startCol + 1})");
+                            return;
+                        }
+                        else if (miner[startRow, startCol + 1] == 'c')
+                        {
+                            collectedCoals++;
+
+                            if (collectedCoals == allCoals)
+                            {
+                                Console.WriteLine($"You collected all coals! ({startRow}, {startCol + 1})");
+                                return;
+                            }
+                            else
+                            {
+                                miner[startRow, startCol + 1] = '*';
+                                startCol = startCol + 1;
+                            }
+                        }
+                        else
+                        {
+                            startCol = startCol + 1;
+                        }
+                    }
+                }
+                else if (command == "left")
+                {
+                    if (IsInside(miner, startRow, startCol - 1))
+                    {
+                        if (miner[startRow, startCol - 1] == 'e')
+                        {
+                            Console.WriteLine($"Game over! ({startRow}, {startCol - 1})");
+                            return;
+                        }
+                        else if (miner[startRow, startCol - 1] == 'c')
+                        {
+                            collectedCoals++;
+
+                            if (collectedCoals == allCoals)
+                            {
+                                Console.WriteLine($"You collected all coals! ({startRow}, {startCol - 1})");
+                                return;
+                            }
+                            else
+                            {
+                                miner[startRow, startCol - 1] = '*';
+                                startCol = startCol - 1;
+                            }
+                        }
+                        else
+                        {
+                            startCol = startCol - 1;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"{allCoals - collectedCoals} coals left. ({startRow}, {startCol})");
+        }
+
+        private static int SumOfCoals(char[,] miner)
+        {
+            int sum = 0;
+
+            for (int row = 0; row < miner.GetLength(0); row++)
+            {
+                for (int col = 0; col < miner.GetLength(1); col++)
+                {
+                    if (miner[row, col] == 'c')
+                    {
+                        sum++;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        private static bool IsInside(char[,] miner, int row, int col)
+        {
+            return row >= 0 && row < miner.GetLength(0) && col >= 0 && col < miner.GetLength(1);
+        }
+    }
+}
