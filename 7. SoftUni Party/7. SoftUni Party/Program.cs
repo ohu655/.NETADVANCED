ab4cd690769581805b38ffa66e6ffd988955ef42
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-09-29 23:37:31 +0300

    Add files via upload

---
+++ 7. SoftUni Party/7. SoftUni Party/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> guests = new HashSet<string>();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "PARTY")
+                {
+                    break;                 
+                }
+                else
+                {
+                    guests.Add(line);
+                }
+
+               
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (guests.Contains(input))
+                {
+                    guests.Remove(input);
+                }
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+
+            }
+            Console.WriteLine(guests.Count);
+
+            foreach (var item in guests)
+            {
+                char[] ch = item.ToCharArray();
+
+                if (char.IsDigit(ch[0]))
+                {
+                    Console.WriteLine(item);
+                }
+                
+            }
+
+            foreach (var item in guests)
+            {
+                char[] ch = item.ToCharArray();
+
+                if (char.IsLetter(ch[0]))
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+        }
+    }
+}
