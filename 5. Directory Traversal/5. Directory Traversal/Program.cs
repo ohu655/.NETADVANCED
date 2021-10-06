5d579c23c91ff9f053f64a90fd81e0ab10f433f4
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-10-06 22:55:27 +0300

    Add files via upload

---
+++ 5. Directory Traversal/5. Directory Traversal/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _5._Directory_Traversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allFiles = Directory.GetFiles(@"..\..\..",".");
+            Dictionary<string, Dictionary<string, double>> groupedFiles = new Dictionary<string, Dictionary<string, double>>();
+
+            foreach (var file in allFiles)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+
+                if (!groupedFiles.ContainsKey(fileInfo.Extension)) 
+                {
+                    groupedFiles.Add(fileInfo.Extension, new Dictionary<string, double>());
+                }
+
+                double size = (double)fileInfo.Length / 1024;
+                groupedFiles[fileInfo.Extension].Add(fileInfo.Name, size);
+            }
+
+            var sorted = groupedFiles.OrderByDescending(n => n.Value.Count).ThenBy(n => n.Key);
+
+            List<string> lines = new List<string>();
+
+            foreach (var item in sorted)
+            {
+                lines.Add(item.Key);
+
+                foreach (var file in item.Value)
+                {
+                    lines.Add($"--{file.Key} - {file.Value:F3}");
+                }
+            }
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/report.txt";
+
+            File.WriteAllLines(path, lines);
+        }
+    }
+}
---
+++ 6. Zip and Extract/6. Zip and Extract/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _6._Zip_and_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory(@".", @"D:\Softuni C#\C# Advanced Projects\Streams Files and Directories\Exercise\6. Zip and Extract\6. Zip and Extract\result.zip");
+        }
+    }
+}
