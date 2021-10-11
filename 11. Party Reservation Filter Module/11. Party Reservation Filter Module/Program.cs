de86448cee7243d6e0918aa522239fe9ec0da6b6
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-10-11 23:12:36 +0300

    Add files via upload

---
+++ 11. Party Reservation Filter Module/11. Party Reservation Filter Module/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._Party_Reservation_Filter_Module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+            var dictionary = new Dictionary<string, Predicate<string>>();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "Print")
+                {
+                    break;
+                }
+
+                string[] parts = line.Split(";");
+                string action = parts[0];
+                string predicateAction = parts[1];
+                string value = parts[2];
+                string key = predicateAction + "_" + value;
+
+                if (action == "Add filter")
+                {
+                    Predicate<string> predicate = GetPredicate(predicateAction, value);
+                    dictionary.Add(key, predicate);
+                }
+                else
+                {
+                    dictionary.Remove(key);
+                }
+            }
+
+            foreach (var (key, predicate) in dictionary)
+            {
+                names.RemoveAll(predicate);
+            }
+
+            Console.WriteLine(string.Join(" ", names));
+        }
+
+        private static Predicate<string> GetPredicate(string commandInfo, string parameter)
+        {
+            if (commandInfo == "Starts with")
+            {
+                return x => x.StartsWith(parameter);
+            }
+
+            if (commandInfo == "Ends with")
+            {
+                return x => x.EndsWith(parameter);
+            }
+
+            if (commandInfo == "Contains")
+            {
+                return x => x.Contains(parameter);
+            }
+
+            int lenght = int.Parse(parameter);
+
+            return x => x.Length == lenght;
+        }
+    }
+}
---
+++ 12. TriFunction/12. TriFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Func<string, int, bool> isLargerOrEqual = (name, asciiSum) => name.Sum(x => x) >= asciiSum;
+
+            Func<List<string>, int, Func<string, int, bool>, string> desiredName = (names, number, func) =>
+            names.FirstOrDefault(x => func(x, number));
+
+            string name = desiredName(names, n, isLargerOrEqual);
+
+            Console.WriteLine(name);
+        }
+    }
+}
