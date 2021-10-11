eeca2a3bcc319a442b14eae5a59b295a2092f705
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-10-11 23:09:34 +0300

    Add files via upload

---
+++ 5. Applied Arithmetics/5. Applied Arithmetics/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int[]> add = numbers =>
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] += 1;
+                }
+                return numbers;
+            };
+            Func<int[], int[]> subtract = numbers =>
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] -= 1;
+                }
+                return numbers;
+            };
+            Func<int[], int[]> multiply = numbers =>
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] *= 2;
+                }
+                return numbers;
+            };
+
+            Action<int[]> print = x => Console.WriteLine(string.Join(" ", x));
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "end")
+                {
+                    break;
+                }
+                else if (command == "add")
+                {
+                    add(input);
+                }
+                else if (command == "subtract")
+                {
+                    subtract(input);
+                }
+                else if (command == "multiply")
+                {
+                    multiply(input);
+                }
+                else if (command == "print")
+                {
+                    print(input);
+                }
+            }
+        }
+    }
+}
---
+++ 6. Reverse and Exclude/6. Reverse and Exclude/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Reverse_and_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int divisibleNumber = int.Parse(Console.ReadLine());
+
+            Func<List<int>, List<int>> divisible = x =>
+            {
+                var result = new List<int>();
+
+                for (int i = 0; i < x.Count; i++)
+                {
+                    if (x[i] % divisibleNumber != 0)
+                    {
+                        result.Add(x[i]);
+                    }
+                }
+
+                return result;
+            };
+
+            var result = divisible(input);
+            result.Reverse();
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
