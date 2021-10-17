ac954407c163bcffc4225e44b48de60d61b4db32
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-10-17 15:45:14 +0300

    Add files via upload

---
+++ CarManufacturerExtension/CarManufacturerExtension/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double consumption = distance * FuelConsumption;
+
+            if (FuelQuantity - consumption > 0)
+            {
+                FuelQuantity -= consumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            string result = $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:f2}";
+
+            return result;
+        }
+
+    }
+}
---
+++ CarManufacturerExtension/CarManufacturerExtension/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "Golf";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 1;
+            car.Drive(100);
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
