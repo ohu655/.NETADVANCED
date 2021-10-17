23f8ef5f323709dc2822522cdc6c9f39c189ab5a
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-10-17 15:21:02 +0300

    Add files via upload

---
+++ CarManufacturer/CarManufacturer/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        private int year;
+
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+    }
+}
