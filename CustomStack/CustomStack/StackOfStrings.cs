4153298a0942ceb1ead6ba14a831952769394efd
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-11-17 22:10:55 +0200

    Add files via upload

---
+++ CustomStack/CustomStack/StackOfStrings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
---
+++ CustomStack/CustomStack/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var stack = new StackOfStrings();
+            stack.Push("23");
+
+            Console.WriteLine(stack.IsEmpty());
+        }
+    }
+}
