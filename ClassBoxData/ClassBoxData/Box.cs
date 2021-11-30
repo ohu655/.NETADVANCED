f817c35c45cf9e66c948a9406337245592778a11
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-11-30 23:01:02 +0200

    Add files via upload

---
+++ ClassBoxData/ClassBoxData/Box.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double lenght;
+        private double width;
+        private double height;
+
+        public Box(double lenght, double width, double height)
+        {
+            Lenght = lenght;
+            Width = width;
+            Height = height;
+        }
+
+        public double Lenght
+        {
+            get
+            {
+                return lenght;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Lenght cannot be zero or negative.");
+                }
+
+                lenght = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Height cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+
+        public double SurfaceArea() => 2 * (Lenght * Width) + 2 * (Lenght * Height) + 2 * (Width * Height);
+
+        public double LateralSurfaceArea() => 2 * (Height * Lenght) + 2 * (Width * Height);
+
+        public double Volume() => Lenght * Height * Width;
+    }
+}
---
+++ FootballTeamGenerator/FootballTeamGenerator/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FootballTeamGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, Team> teams = new Dictionary<string, Team>();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "END")
+                {
+                    break;
+                }
+
+                string[] parts = line.Split(";", StringSplitOptions.RemoveEmptyEntries);
+                string command = parts[0];
+
+                try
+                {
+                    if (command == "Team")
+                    {
+                        string teamName = parts[1];
+                        Team team = new Team(teamName);
+                        teams.Add(teamName, team);
+                    }
+                    else if (command == "Add")
+                    {
+                        string teamName = parts[1];
+
+                        if (!teams.ContainsKey(teamName))
+                        {
+                            Console.WriteLine($"Team {teamName} does not exist.");
+                            continue;
+                        }
+
+                        string playerName = parts[2];
+                        int endurance = int.Parse(parts[3]);
+                        int sprint = int.Parse(parts[4]);
+                        int dribble = int.Parse(parts[5]);
+                        int passing = int.Parse(parts[6]);
+                        int shooting = int.Parse(parts[7]);
+
+                        Player player = new Player(playerName, endurance, sprint, dribble, passing, shooting);
+
+                        teams[teamName].AddPlayer(player);
+
+                    }
+                    else if (command == "Remove")
+                    {
+                        string teamName = parts[1];
+                        string playerName = parts[2];
+
+                        teams[teamName].RemovePlayer(playerName);
+
+                    }
+                    else if (command == "Rating")
+                    {
+                        string teamName = parts[1];
+
+                        if (!teams.ContainsKey(teamName))
+                        {
+                            Console.WriteLine($"Team {teamName} does not exist.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{teamName} - {teams[teamName].Rating}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
